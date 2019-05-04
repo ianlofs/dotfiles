aa39366fda889e1434fab486826c141d8023f59f
Ian Lofgren <ian@ianlofgren.com>
2019-05-04 10:18:33 -0600

    make sure to ignore the gpg stuff that is secret

--- install.sh
+++ install.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
-set -e
+set -e -ou pipefail
 
-for i in _*; do
-    source="${PWD}/$i"
-    target="${HOME}/${i/_/.}"
-    
-    # everything else gets symlinked into home dir
-    if [ -e "${target}" ] && [ ! -h "${target}" ]; then
-        backup="${HOME}/.save.${i}"
-        echo "${target} already exists. Moving to ${backup}"
-        mv "${target}" "${backup}"
-    fi
-    ln -s "$source" "$target"
-done
+
+function setup_link() {
+    # for dotfiles in the home dir
+    for i in _*; do
+        source="${PWD}/$i"
+        target="${HOME}/${i/_/.}"
+        
+        # everything else gets symlinked into home dir
+        if [ -e "${target}" ] && [ ! -h "${target}" ]; then
+            backup="${HOME}/.save.${i}"
+            echo "${target} already exists. Moving to ${backup}"
+            mv "${target}" "${backup}"
+        fi
+        ln -s "$source" "$target"
+    done
+
+    # kitty terminal conf
+    ln -s "${PWD}/kitty.conf" "$HOME/.config/kitty/kitty.conf"
+}
