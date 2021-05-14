9582b36104a0dfa43d1386a069ecb559232595d7
Ian Lofgren <ian@ianlofgren.com>
2021-05-13 22:58:53 -0600

    updates to install script and rc files

--- install.sh
+++ install.sh
@@ -1,15 +1,90 @@
 #!/usr/bin/env bash
 set -e -ou pipefail
 
+PACKAGES=(
+    go
+    python
+    pyenv
+    cookiecutter
+    grep
+    gnu-sed
+    gnu-tar
+    base64
+    gnu-zsh
+    gnu-getopt
+    curl
+    fzf
+    git
+    macvim
+    readline
+    gnupg
+    unzip
+    gzip
+    libpq
+    openssl
+    yq@3
+    helm
+    kubectx
+    tig
+)
 
-function setup_link() {
-    # for dotfiles in the home dir
+CASKS=(
+    kitty
+    zoom
+    slack
+    spotify
+    visual-studio-code
+    1password
+    firefox
+    gpg-suite
+    snowflake-snowsql
+)
+
+function install_prereqs () {
+    echo "Installing prereqs"
+    # xcode tools needed for homebrew
+    xcode-select --install
+
+    # homebrew, used for everything else
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo "Done installing prereqs"
+}
+
+function install_packages () {
+    echo "Installing homebrew packages..."
+    brew update && brew reinstall "${PACKAGES[@]}"
+    
+    /usr/local/opt/fzf/install --key-bindings --completion --no-update-rc
+    echo "Done install homebrew packages."   
+}
+
+function install_applications () {
+    echo "Installing homebrew applications..."
+    brew update && brew install --cask "${CASKS[@]}"
+    echo "Done installing homebrew applications."   
+}
+
+function setup_shell () {
+    echo "Setting up powerline-go ..."
+    go get -u github.com/justjanne/powerline-go
+    curl https://raw.githubusercontent.com/powerline/fonts/master/install.sh | bash
+    echo "Done setting up powerline-go."
+}
+
+function setup_python () {
+    echo "Setting up poetry ..."
+    curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+    echo "Done setting up poetry."
+}
+
+function setup_dotfiles () {
+    # setup homedir dotfiles
     for i in _*; do
         source="${PWD}/$i"
         target="${HOME}/${i/_/.}"
-        
+
         # everything else gets symlinked into home dir
-        if [ -e "${target}" ] && [ ! -h "${target}" ]; then
+        if [ -e "${target}" ] && [ -h "${target}" ]; then
             backup="${HOME}/.save.${i}"
             echo "${target} already exists. Moving to ${backup}"
             mv "${target}" "${backup}"
@@ -20,3 +95,14 @@ function setup_link() {
     # kitty terminal conf
     ln -s "${PWD}/kitty.conf" "$HOME/.config/kitty/kitty.conf"
 }
+
+function main () {
+    install_prereqs
+    install_packages
+    install_applications
+    setup_shell
+    setup_python
+    setup_dotfiles
+}
+
+main
